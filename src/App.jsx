9b2b596ae7c574bc8c9197557aefbf678a5b4540
vectorNull <jose.rodriguez76@protonmail.com>
2023-11-18 10:38:38 -0800

    Added userList component and redux config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import UsersList from './components/UsersList';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   return (
-    <div>App</div>
+    <Provider store={store}>
+      <div className='container mx-auto px-4'>
+        <UsersList />
+      </div>
+    </Provider>
   )
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/userSlice";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer
+  }
+});
+
+export {
+  store,
+  usersReducer,
+}
+
